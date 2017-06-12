2dc4676c9dbe95c2f35b6fd9f530a9cde393e63e
Xhriman <seal789ie@gmail.com>
2017-06-13 00:43:48 +0800

    upd

--- LeetCode/84. Largest Rectangle in Histogram.playground/Contents.swift
+++ LeetCode/84. Largest Rectangle in Histogram.playground/Contents.swift
@@ -17,27 +17,27 @@ func largestRectangleArea(_ heights: [Int]) -> Int {
 //    r - is the last coordinate of the bar to the right with height h[r] >= h[i]
 //    l - is the last coordinate of the bar to the left which height h[l] >= h[i]
     
-    var lastLeftBarHigherThanBar: [Int] = Array(repeating: -1, count: numBar)
+    var firstLeftBarLowerThanBar: [Int] = Array(repeating: -1, count: numBar)
     for i in 1 ..< numBar {
-        var p = i - 1
-        while p >= 0 && heights[p] >= heights[i] {
-            p = lastLeftBarHigherThanBar[p]
+        var left = i - 1
+        while left >= 0 && heights[left] >= heights[i] {
+            left = firstLeftBarLowerThanBar[left]
         }
-        lastLeftBarHigherThanBar[i] = p
+        firstLeftBarLowerThanBar[i] = left
     }
     
-    var lastRightBarHigherThanBar: [Int] = Array(repeating: numBar, count: numBar)
+    var firstRightBarLowerThanBar: [Int] = Array(repeating: numBar, count: numBar)
     for i in stride(from: numBar - 2, through: 0, by: -1) {
-        var p = i + 1
-        while p < numBar && heights[p] >= heights[i] {
-            p = lastRightBarHigherThanBar[p]
+        var right = i + 1
+        while right < numBar && heights[right] >= heights[i] {
+            right = firstRightBarLowerThanBar[right]
         }
-        lastRightBarHigherThanBar[i] = p
+        firstRightBarLowerThanBar[i] = right
     }
     
     var maxArea = 0
     for i in 0 ..< numBar {
-        let width = lastRightBarHigherThanBar[i] - lastLeftBarHigherThanBar[i] - 1
+        let width = firstRightBarLowerThanBar[i] - firstLeftBarLowerThanBar[i] - 1
         maxArea = max(maxArea, heights[i] * width)
     }
     return maxArea
